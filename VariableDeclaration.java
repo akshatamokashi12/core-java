62dcb6e330720b9c43d3f33874b29e2c0da48969
akshatamokashi12 <137081805+akshatamokashi12@users.noreply.github.com>
2023-06-20 12:11:03 +0530

    Add files via upload

---
+++ VariableDeclaration.java
@@ -0,0 +1,201 @@
+public class VariableDeclaration {
+
+    public static void main(String[] args) {
+        // byte variables
+        byte temperature = 25;
+        byte age = 30;
+        byte marks = 85;
+        byte quantity = 10;
+        byte employees = 5;
+        byte days = 7;
+        byte siblings = 2;
+        byte pages = 100;
+        byte hours = 24;
+        byte months = 12;
+
+        
+        System.out.println("Temperature: " );
+        System.out.println("Age: " );
+        System.out.println("Marks: " );
+        System.out.println("Quantity: " );
+        System.out.println("Employees: " );
+        System.out.println("Days: " );
+        System.out.println("Siblings: " );
+        System.out.println("Pages: " );
+        System.out.println("Hours: " );
+        System.out.println("Months: " );
+       
+
+        // short variables
+        short population = 15000;
+        short distance = 2500;
+        short score = 450;
+        short width = 1200;
+        short height = 800;
+        short capacity = 200;
+        short voltage = 220;
+        short speed = 200;
+        short level = 5;
+        short temperatureRange = 100;
+
+        
+        System.out.println("Population: " );
+        System.out.println("Distance: ");
+        System.out.println("Score: ");
+        System.out.println("Width: " );
+        System.out.println("Height: " );
+        System.out.println("Capacity: " );
+        System.out.println("Voltage: " );
+        System.out.println("Speed: " );
+        System.out.println("Level: " );
+        System.out.println("Temperature Range: ");
+       
+
+        // int variables
+        int populationCount = 1000000;
+        int distanceTravelled = 50000;
+        int totalScore = 1500;
+        int screenWidth = 1920;
+        int screenHeight = 1080;
+        int bankBalance = 100000;
+        int studentCount = 500;
+        int ageLimit = 18;
+        int timeElapsed = 120;
+        int maximumValue = 1000;
+
+        
+        System.out.println("Population Count: " );
+        System.out.println("Distance Travelled: " );
+        System.out.println("Total Score: " );
+        System.out.println("Screen Width: " );
+        System.out.println("Screen Height: " );
+        System.out.println("Bank Balance: " );
+        System.out.println("Student Count: " );
+        System.out.println("Age Limit: ");
+        System.out.println("Time Elapsed: " );
+        System.out.println("Maximum Value: " );
+        
+
+        // long variables
+        long populationCountWorld = 7800000000L;
+        long distanceToMoon = 384400000L;
+        long totalScoreBoard = 9999999999L;
+        long screenWidthPixels = 25601440L;
+        long screenHeightPixels = 14401440L;
+        long bankAccountBalance = 1000000000L;
+        long totalSeconds = 3600000L;
+        long contactNumber = 1234567890L;
+        long millisecondsElapsed = 60000L;
+        long maxPopulation = 10000000000L;
+
+        
+        System.out.println("Population Count World: " );
+        System.out.println("Distance to Moon: " );
+        System.out.println("Total Score Board: " );
+        System.out.println("Screen Width Pixels: " );
+        System.out.println("Screen Height Pixels: " );
+        System.out.println("Bank Account Balance: " );
+        System.out.println("Total Seconds: " );
+        System.out.println("Contact Number: " );
+        System.out.println("Milliseconds Elapsed: " );
+        System.out.println("Max Population: " );
+        
+
+        // float variables
+        float weight = 65.5f;
+        float heightInMeter = 1.75f;
+        float temperatureInCelsius = 37.5f;
+        float piValue = 3.14f;
+        float velocity = 9.8f;
+        float distanceInKm = 15.5f;
+        float GPA = 3.75f;
+        float pressure = 1013.25f;
+        float timeInSeconds = 30.5f;
+
+       
+        System.out.println("Weight: " );
+        System.out.println("Height in Meter: " );
+        System.out.println("Temperature in Celsius: " );
+        System.out.println("Pi Value: " );
+        System.out.println("Velocity: " );
+        System.out.println("Distance in Km: " );
+        System.out.println("GPA: " );
+        System.out.println("Pressure: " );
+        System.out.println("Time in Seconds: " );
+       
+
+        // double variables
+        double weightInKg = 75.5;
+        double heightInCm = 180.2;
+        double temperatureInFahrenheit = 98.6;
+        double pi = 3.141592653589793;
+        double velocityInMph = 65.7;
+        double distanceInMiles = 10.5;
+        double GPAAverage = 3.9;
+        double pressureInPa = 101325.0;
+        double timeInMilliseconds = 500.3;
+        double priceInDollars = 99.99;
+
+        
+        System.out.println("Weight in Kg: " );
+        System.out.println("Height in Cm: " );
+        System.out.println("Temperature in Fahrenheit: " );
+        System.out.println("Pi: " );
+        System.out.println("Velocity in Mph: " );
+        System.out.println("Distance in Miles: " );
+        System.out.println("GPA Average: " ;
+        System.out.println("Pressure in Pa: " );
+        System.out.println("Time in Milliseconds: " );
+        System.out.println("Price in Dollars: " );
+       
+
+        // char variables
+        char gender = 'M';
+        char grade = 'A';
+        char currencySymbol = '$';
+        char section = 'B';
+        char division = 'C';
+        char alphabet = 'Z';
+        char symbol = '+';
+        char status = 'O';
+        char mode = 'E';
+        char letter = 'X';
+
+        
+        System.out.println("Gender: " );
+        System.out.println("Grade: " );
+        System.out.println("Currency Symbol: " );
+        System.out.println("Section: " );
+        System.out.println("Division: ");
+        System.out.println("Alphabet: " );
+        System.out.println("Symbol: " );
+        System.out.println("Status: ");
+        System.out.println("Mode: " );
+        System.out.println("Letter: " );
+        
+
+        // boolean variables
+        boolean isRaining = true;
+        boolean isSunny = false;
+        boolean isCold = true;
+        boolean hasPassedExam = true;
+        boolean isLoggedIn = false;
+        boolean isAvailable = true;
+        boolean isClosed = false;
+        boolean isStarted = true;
+        boolean hasFinished = false;
+        boolean isCorrect = true;
+
+        
+        System.out.println("isRaining");
+        System.out.println("Is Sunny");
+        System.out.println("Is Cold");
+        System.out.println("Has Passed Exam: " + hasPassedExam);
+        System.out.println("Is Logged In: " + isLoggedIn);
+        System.out.println("Is Available: " + isAvailable);
+        System.out.println("Is Closed: " + isClosed);
+        System.out.println("Is Started: " + isStarted);
+        System.out.println("Has Finished: " + hasFinished);
+        System.out.println("Is Correct: " + isCorrect);
+    }
+}
